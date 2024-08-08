7eaf053f2351d304161a5d504e0fd7793dd40310
rebecapologini <>
2024-08-08 19:11:20 +0300

    home component changed

--- client/src/pages/home/home-component.tsx
+++ client/src/pages/home/home-component.tsx
@@ -6,10 +6,10 @@ import { RightOutlined } from "@ant-design/icons";
 
 import Header from "../../components/header/header-component.tsx";
 import { useNavigate } from "react-router-dom";
+import { useFetchUserQuery } from "../../features/auth/auth-api-slice.ts";
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
-
   return (
     <div className="welcome-page">
       <Header />
@@ -17,12 +17,12 @@ const Home: React.FC = () => {
         <Card
           className="card hoverable-card"
           cover={
-            <div className="image-container">
+            <div className="image-container" style={{ marginLeft: 25 + "px" }}>
               <img alt="example" src="https://i.ibb.co/8c68s3M/image.png" />
             </div>
           }
           bordered={true}
-          style={{ width: 225, height: 415 }}
+          style={{ width: 250, height: 415 }}
         >
           <div className="card-content">
             <h3>Новый способ поиска преподавателей</h3>
@@ -30,6 +30,7 @@ const Home: React.FC = () => {
               type="details"
               text="Подробнее"
               iconAfterText={<RightOutlined />}
+              disabled={false}
             />
           </div>
         </Card>
@@ -40,12 +41,14 @@ const Home: React.FC = () => {
           text="Войти"
           block
           onClick={() => navigate("/auth?form=login")}
+          disabled={false}
         />
         <CustomButton
           type="tertiary"
           text="Регистрация"
           block
           onClick={() => navigate("/auth?form=register")}
+          disabled={false}
         />
       </div>
     </div>
